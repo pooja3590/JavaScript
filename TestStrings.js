66116adab7e98713e333c8703ed43ddc02d0c2e5
pooja3590 <poojamutreja532@gmail.com>
2024-07-11 21:34:46 +0530

    teststrings

---
+++ TestStrings.js
@@ -0,0 +1,71 @@
+let Surname = "rani"
+let F1name = "Pooja"
+let DOB = 30
+let Age = 35
+//console.log (Surname)
+console.log (typeof Surname)
+console.log (typeof F1name)
+console.log (typeof DOB)
+
+console.log(DOB+Age+Surname)
+console.log(DOB+Age+Surname+F1name)
+
+console.log(F1name+DOB+Age+Surname)
+//console.log(F1name+Surname+DOB+Age)
+
+console.log ("My Sister first name is "+F1name+" and Last name is "+Surname+"")
+
+console.log (`My Sister first name is ${F1name} and Last name is ${Surname}`)
+
+//let CountryL1 = ["USA","Europe","Australia","New zealand","Canada","UAE"]
+//console.log (CountryL1[0])
+//console.log (CountryL1.length)
+
+let countryL2 = "Australia"
+console.log(countryL2[0])
+console.log(countryL2.length)
+
+
+//for (i=0;i<countryL2.length;i++){
+ //   console.log (countryL2[i])
+//}
+
+//for (i=countryL2.length-1;i>=0;i--){
+  //  console.log (countryL2[i])
+//}
+
+let i=0
+while(i < countryL2.length){
+    //console .log(i)
+    console.log(countryL2[i])
+    i = i+1
+}
+
+let i1=countryL2.length-1
+while(i1>=0){
+    //console .log(i)
+    console.log(countryL2[i1])
+    i1 = i1-1
+}
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
