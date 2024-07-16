ce3d611de38a6503f9daba667957665a1bec944d
pooja3590 <poojamutreja532@gmail.com>
2024-07-16 08:19:40 +0530

    test15jsaddednew

---
+++ Test15.js
@@ -0,0 +1,31 @@
+let students = [
+    {
+        firstName:"chinmay",
+        lastName:"deshpande",
+        age:34,
+        skills:["sql"]
+    },
+    {
+        firstName:"poorva",
+        lastName:"vyas",
+        age:29,
+        skills:["javascript","django","cpp"]
+    },
+    {
+        firstName:"amol",
+        lastName:"rao",
+        age:32,
+        skills:["python","javascript","sql","power BI"]
+    },
+    {
+        firstName:"amit",
+        lastName:"bhure",
+        age:28,
+        skills:["python","jquery"]
+    }
+
+]
+
+students.forEach(function(el)){
+    console.log(students.firstName)
+}
