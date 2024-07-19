710bf499f610c6e87c4d789139fd9a93c64ac982
pooja3590 <poojamutreja532@gmail.com>
2024-07-19 20:54:36 +0530

    test

---
+++ DOM/Script.js
@@ -0,0 +1,9 @@
+//document.querySelector('h1').addEventListener('click',function(){
+   // document.querySelector('h1').style.color = "green"
+//})
+
+//let bytagname = document.querySelector(h1)
+//console.log(bytagname)
+
+let byclassname = Document.querySelector(12)
+console.log(byclassname)
