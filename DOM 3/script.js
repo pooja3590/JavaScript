5b546807e78dcf744a8be33ac67bc3d32ad8130f
pooja3590 <poojamutreja532@gmail.com>
2024-07-23 21:26:25 +0530

    DOM4

--- DOM 3/script.js
+++ DOM 3/script.js
@@ -1,6 +1,19 @@
 let Heading = document.querySelector(h1)
-console.log(heading)
-let Inputtag = document.querySelector(input)
-console.log(inputtag)
+console.log(Headingeading)
+let Inputtext = document.querySelector(input)
+console.log(Inputtext)
 let ButtonB1 = document.querySelector(button)
 console.log (ButtonB1)
+
+ButtonB1.addEventListener('click',function(){
+    let Colortext = Inputtext.value
+    Headingeading.style.color = Colortext
+})
+
+
+
+//buttonT.addEventListener('click',function(){
+   // let colorText = inputT.value
+   // headT.style.color = colorText
+   // inputT.value = ""
+//})
---
+++ DOM4/script.js
@@ -0,0 +1,9 @@
+let inputT =document.querySelector('input')
+console.log(inputT)
+
+let buttonB2 = document.querySelector('button')
+console.log(buttonB2)
+
+let listL1 = document.querySelector('ul')
+console.log(listL1)
+
