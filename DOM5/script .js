fa2091ff18184a1ede2a94f1f2a396ab762fef60
pooja3590 <poojamutreja532@gmail.com>
2024-07-25 22:20:06 +0530

    dom5 craeted

---
+++ DOM5/script .js
@@ -0,0 +1,8 @@
+let textbox = Document.queryselector('input')
+console.log (textbox)
+
+let buttonB3 = document.querySelector('button')
+console.log (buttonB3)
+
+let listL2 = document.querySelector('ul')
+console.log (listL2)
