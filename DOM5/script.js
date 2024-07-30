5c073798ffd76b7afb91d5f7c37f07411bf66217
pooja3590 <poojamutreja532@gmail.com>
2024-07-30 23:24:16 +0530

    DOM6 created

--- DOM5/script.js
+++ DOM5/script.js
@@ -11,14 +11,33 @@ classoneB1.addEventListener('click',function(){
     let newLi = document.createElement('li')
     let valuei = textbox.value
     newLi.textContent= valuei
-    createbuttons(newLi)
+    createButtons(newLi)
     listL2.append(newLi)
     textbox.value =""
 })
 
 /*creating buttons new added list element*/
 
+function createButtons(li){
+    let r = document.createElement('button') // <button></button>
+    r.textContent = "Remove" // <button>Remove</button>
+    r.classList.add('remove') // <button class = "remove">Remove</button>
+    li.appendChild(r)
+}
 
+function createButtons(li){
+    let U= document.createElement('button')
+    U.textContent='Up'
+    U.classList.add('Up')
+    li.appendChild(U)
 
+}
+
+function createButtons(li){
+    let D= document.createElement('button')
+    D.textContent ='Down'
+    D.classList.add('down')
+    li.appendChild(D)
+}
 
 
---
+++ DOM6/script.js
@@ -0,0 +1,28 @@
+let a1= document.querySelector('h1')
+console.log(a1)
+
+let a2= document.querySelector('#new')
+console.log(a2)
+
+let a3 =document.querySelector('.one')
+console.log(a3)
+
+let a4= document.querySelector('h1[name="old"]')
+console.log(a4)
+
+let li =document.querySelector('li')
+console.log(li)
+
+let allli = document.querySelectorAll('li')
+console.log(allli)
+
+let AL1 = document.querySelectorAll('[name="old"]')
+console.log(AL1)
+
+
+for (i=0; i<allli.length;i++){
+    console.log(i)
+    console.log(allli.length[i].textContent)
+    allli.length[i].style.color="red"
+
+}
